4694bc0f959eaaf9fd7bc3a440a40696981b6aa1
Sediq <sediqabdullahi04@gmail.com>
2023-09-28 13:17:21 +0100

    Added an update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Naijawives from "./components/naijawives";
+import Scroll from "./components/scroll";
 export default () => {
   return (
     <div className="">
       <Router>
+        <Scroll />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/9jawives" element={<Naijawives />} />
